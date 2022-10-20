dba599cbf070e626ccdf3dc20db0325f559b43e7
LindaSieb <linda.siebert@sap.com>
2022-10-20 17:26:24 +0200

    Add google chrome options

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,6 +57,16 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
+        'goog:chromeOptions': {
+             args: [
+                 '--no-sandbox',
+                 '--disable-infobars',
+                 '--headless',
+                 '--disable-gpu',
+                 '--disable-dev-shm-usage',
+                 '--window-size=1440,735'
+            ],
+        },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
